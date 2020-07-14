5411f42510068e79f1eac8f1d26870722903a384
Jacob Gear <geaja121@student.otago.ac.nz>
2020-07-14 13:06:37 +1200

    Created a Customer domain class,
    References #1.

---
+++ info202_project/src/main/java/domain/Customer.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+/**
+ *
+ * @author geaja121
+ */
+public class Customer {
+	Integer customerID;
+	String username;
+	String firstName;
+	String surname;
+	String password;
+	String emailAddress;
+	String shippingAddress;
+}
