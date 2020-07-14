3885d3094423c64049f136bef2877a85686a34ba
Jacob Gear <geaja121@student.otago.ac.nz>
2020-07-14 12:33:01 +1200

    Created a Project domain class,
    References #1.

---
+++ info202_project/src/main/java/domain/Product.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author geaja121
+ */
+public class Product {
+	String productID;
+	String name;
+	String description;
+	String category;
+	BigDecimal listPrice;
+	BigDecimal quantityInStock;
+
+	public String getProductID() {
+		return productID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public BigDecimal getListPrice() {
+		return listPrice;
+	}
+
+	public BigDecimal getQuantityInStock() {
+		return quantityInStock;
+	}
+
+	public void setProductID(String productID) {
+		this.productID = productID;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public void setListPrice(BigDecimal listPrice) {
+		this.listPrice = listPrice;
+	}
+
+	public void setQuantityInStock(BigDecimal quantityInStock) {
+		this.quantityInStock = quantityInStock;
+	}
+
+	@Override
+	public String toString() {
+		return "Product{" + "productID=" + productID + ", name=" + name + '}';
+	}
+	
+}
