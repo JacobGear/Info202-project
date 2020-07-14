5d454ec723b6657a963424e0f662d3c29d5c37a2
Jacob Gear <geaja121@student.otago.ac.nz>
2020-07-14 13:08:26 +1200

    Created a Customer domain class,
    References #1.

--- info202_project/src/main/java/domain/Customer.java
+++ info202_project/src/main/java/domain/Customer.java
@@ -17,4 +17,62 @@ public class Customer {
 	String password;
 	String emailAddress;
 	String shippingAddress;
+
+	public Integer getCustomerID() {
+		return customerID;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public String getShippingAddress() {
+		return shippingAddress;
+	}
+
+	public void setCustomerID(Integer customerID) {
+		this.customerID = customerID;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public void setShippingAddress(String shippingAddress) {
+		this.shippingAddress = shippingAddress;
+	}
+	
+	
 }
