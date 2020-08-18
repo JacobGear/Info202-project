f30a63bc363ae8b98d3d54533e9871176233feba
Admin <Admin@192.168.20.12>
2020-08-18 19:59:03 +1200

    Added equals and hash code methods to Product domain

--- info202_project/src/main/java/domain/Product.java
+++ info202_project/src/main/java/domain/Product.java
@@ -6,6 +6,7 @@
 package domain;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 import net.sf.oval.constraint.Length;
 import net.sf.oval.constraint.NotBlank;
 import net.sf.oval.constraint.NotNegative;
@@ -106,5 +107,31 @@ public class Product {
 	public String toString() {
 		return "Product: " + "productID =" + productID + ", name =" + name;
 	}
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 53 * hash + Objects.hashCode(this.productID);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Product other = (Product) obj;
+        if (!Objects.equals(this.productID, other.productID)) {
+            return false;
+        }
+        return true;
+    }
 	
+        
 }
