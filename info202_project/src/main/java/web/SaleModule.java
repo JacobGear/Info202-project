f74a90233ab21ee3eb9cb69cf176e6f600038a2b
Admin <Admin@192.168.20.12>
2020-09-08 11:23:55 +1200

    Added a SaleModule, but still need to add the uri for creating sales
    References #27.

---
+++ info202_project/src/main/java/web/SaleModule.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package web;
+
+import org.jooby.Jooby;
+
+/**
+ *
+ * @author Admin
+ */
+public class SaleModule extends Jooby{
+    
+     public SaleModule(){
+        
+    }
+}
