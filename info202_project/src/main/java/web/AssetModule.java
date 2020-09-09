25155863b0508e3bb52d8b2966c5d20860ebe7c2
Admin <Admin@192.168.20.12>
2020-09-09 18:21:41 +1200

    Fixed the AssetModule bug from lab 8

--- info202_project/src/main/java/web/AssetModule.java
+++ info202_project/src/main/java/web/AssetModule.java
@@ -14,18 +14,18 @@ import org.jooby.Results;
  */
 public class AssetModule extends Jooby {
 
-   public AssetModule() {
-      assets("/.html");
-      assets("/css/.css");
-      assets("/js/.js");
-      assets("/images/.png");
-      assets("/images/.jpg");
+    public AssetModule() {
+        assets("/*.html");
+        assets("/css/*.css");
+        assets("/js/*.js");
+        assets("/images/*.png");
+        assets("/images/*.jpg");
 
-      // make index.html the default page
-      assets("/", "index.html");
+        // make index.html the default page
+        assets("/", "index.html");
 
-      // prevent 404 errors due to browsers requesting favicons
-      get("/favicon.ico", () -> Results.noContent());
+        // prevent 404 errors due to browsers requesting favicons
+        get("/favicon.ico", () -> Results.noContent());
     }
 
 }
