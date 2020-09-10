768c2f97def2d1f4aa5053f1d7fabdc4f95281c2
Admin <Admin@192.168.20.12>
2020-09-10 20:51:44 +1200

    CustomerModule now gives status not found when the username of a customer gives a null customer

--- info202_project/src/main/java/web/CustomerModule.java
+++ info202_project/src/main/java/web/CustomerModule.java
@@ -3,6 +3,7 @@ package web;
 import dao.CustomerDAO;
 import domain.Customer;
 import org.jooby.Jooby;
+import org.jooby.Result;
 import org.jooby.Status;
 
 /**
@@ -13,10 +14,14 @@ public class CustomerModule extends Jooby{
     
      public CustomerModule(CustomerDAO customerDao){
    
-        get("/api/customers/:username", (req) -> {
-            String username = req.param("username").value();
-            return customerDao.getCustomer(username);
-        });
+         get("/api/customers/:username", (req) -> {
+             String username = req.param("username").value();
+             if (customerDao.getCustomer(username) == null) {
+                 return new Result().status(Status.NOT_FOUND);
+             } else {
+                 return customerDao.getCustomer(username);
+             }
+         });
         
         post("/api/register", (req, rsp) -> {
             Customer customer = req.body().to(Customer.class);
