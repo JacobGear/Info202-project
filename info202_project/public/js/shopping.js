94bd186c2418eeb8ad2a48a5b4ffdb5e4871c9f2
Admin <Admin@192.168.20.12>
2020-09-10 20:53:43 +1200

    Added Product controllers and Customer controllers, that controls the dynamic content in the web pages.

--- info202_project/public/js/shopping.js
+++ info202_project/public/js/shopping.js
@@ -10,10 +10,70 @@ module.factory('productAPI', function ($resource) {
 module.factory('categoryAPI', function ($resource) {
     return $resource('/api/categories/:cat');
 });
+module.factory('registerAPI', function ($resource) {
+    return $resource('/api/register');
+});
+module.factory('signInAPI', function ($resource) {
+   return $resource('/api/customers/:username');
+});
+module.factory('productsAPI', function ($resource) {
+    return $resource('/api/products/');
+});
 
 // first controller
-module.controller('ProductController', function (productAPI) {
+module.controller('ProductController', function (productAPI, categoryAPI, productsAPI) {
     //alert("in controller");
     this.products = productAPI.query();
-})
+    this.categories = categoryAPI.query();
+
+    // click handler for the category filter buttons
+    this.selectCategory = function (selectedCat) {
+        this.products = categoryAPI.query({"cat": selectedCat});
+    };
+    
+    this.allProducts = function () {
+        this.products = productsAPI.query();
+    };
+    
+     
+    
+   
+});
+module.controller('CustomerController', function (registerAPI, $window, signInAPI, $sessionStorage) {
+    this.registerCustomer = function (customer) {
+        registerAPI.save(null, customer,
+            // success callback
+            function () {
+                $window.location = 'signin.html';
+            },
+            // error callback
+            function (error) {
+                console.log(error);
+            }
+        );
+    };
+    this.signInMessage = "Please sign in to continue.";
+    
+            // alias 'this' so that we can access it inside callback functions
+            let ctrl = this;
+
+            this.signIn = function (username, password) {
+
+                // get customer from web service
+                signInAPI.get({'username': username},
+                        // success callback
+                                function (customer) {
+                                    // also store the retrieved customer
+                                    $sessionStorage.customer = customer;
+
+                                    // redirect to home
+                                    $window.location = '.';
+                                },
+                                // fail callback
+                                        function () {
+                                            ctrl.signInMessage = 'Sign in failed. Please try again.';
+                                        }
+                                );
+                            };
+});
 
