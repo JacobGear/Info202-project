addf9600709df5ab6b90436ed8ad1bf2b1ad4f44
Admin <Admin@192.168.20.12>
2020-09-27 23:54:52 +1300

    Added a mail service when a user checks out. Uses the SaleItem collection toString which is ugly which is why this issue is not yet closed.
    References #33.

--- info202_project/src/main/java/web/SaleModule.java
+++ info202_project/src/main/java/web/SaleModule.java
@@ -6,7 +6,15 @@
 package web;
 
 import dao.SaleDAO;
+import domain.Customer;
 import domain.Sale;
+import domain.SaleItem;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.CompletableFuture;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
 import org.jooby.Jooby;
 import org.jooby.Status;
 
@@ -18,12 +26,33 @@ public class SaleModule extends Jooby{
     
      public SaleModule(SaleDAO saleDao){
          
-        post("/api/sales", (req, rsp) -> {
-            Sale sale = req.body().to(Sale.class);
-            System.out.println(sale);
-            saleDao.save(sale);
-            rsp.status(Status.CREATED);
-        });
+         post("/api/sales", (req, rsp) -> {
+             Sale sale = req.body().to(Sale.class);
+             Customer c = sale.getCustomer();
+             Collection<SaleItem> items = new ArrayList<>();
+             Collection<SaleItem> si = sale.getItems();
+             System.out.println(sale);
+             saleDao.save(sale);
+
+             CompletableFuture.runAsync(() -> {
+                 try {
+                     Email email = new SimpleEmail();
+                     email.setHostName("localhost");
+                     email.setSmtpPort(2525);
+                     email.setFrom("FruitShop@gmail.com");
+                     email.setSubject("Thank you for you purchase"
+                             + c.getUsername() + "- Fruit Shop");
+                     email.setMsg("Hello " + c.getFirstName() + ",\n" + si);
+                     email.addTo(c.getEmailAddress());
+                     email.send();
+                 } catch (EmailException ee) {
+                     System.out.println("Email exception");
+                 }
+             });
+             System.in.read();
+
+             rsp.status(Status.CREATED);
+         });
         
     }
 }
